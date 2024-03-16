c655f450bfef1fd5879621ac84cf96dbad045bed
thumbzzero <thumbzzero@gmail.com>
2024-03-16 19:49:54 +0900

    icon config 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `https://avatars.githubusercontent.com/u/94180099?v=4`, // This path is relative to the root of the site.
+        icon: `src/images/profile-pic.png`, // This path is relative to the root of the site.
       },
     },
   ],
