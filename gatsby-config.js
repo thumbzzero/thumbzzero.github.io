1aa73303aa1b349113d91453848646c5b1759d5e
thumbzzero <thumbzzero@gmail.com>
2026-05-19 22:05:49 +0900

    feat: gatsby-remark-external-links 설정 (링크 새 창으로 열리도록)

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,13 @@ module.exports = {
             },
           },
           `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-external-links`,
+            options: {
+              target: `_blank`,
+              rel: `noopener noreferrer`,
+            },
+          },
         ],
       },
     },
